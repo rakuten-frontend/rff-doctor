a4779168ac55c06ac21f71f6cd7cd88bac5e4402
Tanjo, Hiroyuki <hiroyuki.tanjo@mail.rakuten.com>
2014-11-13 11:13:35 +0900

    Support 'npm test' command

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
 
   });
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['test']);
 
 };
